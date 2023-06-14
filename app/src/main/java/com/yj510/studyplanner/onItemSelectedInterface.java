0d3c0ada5b7237770864ba76be55df3d2d36ff22
YJ510 <anni20@knu.ac.kr>
2023-06-15 03:45:58 +0900

    RecyclerView setOnClickListener 구현
    [참고]
    꾸준함의 기록 | [Android java] recyclerview setOnClickListener 두 가지 방법
    https://abcdefgh123123.tistory.com/264

---
+++ app/src/main/java/com/yj510/studyplanner/onItemSelectedInterface.java
@@ -0,0 +1,9 @@
+package com.yj510.studyplanner;
+
+import android.view.View;
+
+import java.util.ArrayList;
+
+public interface onItemSelectedInterface{
+    void onItemSelected(View view, ArrayList<Date> date, int position);
+}
